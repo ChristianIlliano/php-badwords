eadf188898718bf91d4c90364c60434582e5a0a1
ChristianIlliano <grafica.illiano@gmail.com>
2024-05-20 14:53:50 +0200

    first commit

---
+++ greeting.php
@@ -0,0 +1,25 @@
+<?php
+ $paragraph = $_GET["paragraph"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h2>Ecco la lunghezza del tuo paragrafo</h2>
+        <p>Questa è la sua lunghezza <?php echo strlen($paragraph); ?> </p>
+    </section>
+
+    <section>
+        <h2>Ecco il un paragrafo censurato</h2>
+        <!-- (find,replace,string,count) -->
+        <p><?php echo str_replace("hey","ciao",$paragraph); ?> </p>
+    </section>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Scrivi il un paragrafo</h2>
+
+    <form action="greeting.php" method="GET">
+        <input type="text" name="paragraph">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
